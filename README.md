c5b77e276c1af0fd70ec792bdeb3ce1bc26e9ec4
Daniel Hoffmann <daniel.hoffmann@pubmatic.com>
2021-02-23 14:40:45 +1100

    basic working version

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # audience-encore-csv-parser
 
-A simple way to parse a correctly formatted .csv file in order to populate a segment sitting in PubMatic's Audience Encore with audience data.
+A simple way to parse a correctly formatted .tsv file (might add csv later) in order to populate a segment sitting in PubMatic's Audience Encore with audience data.
 
 ## Requirements
 
 - Node
+
+## How to
+
+Run
+
+    npm install
+
+to install all dependencies.
+
+Then add tsv file to 'data' directory and update config.fileName in app.js.
+
+Finally, run
+
+    node app.js
+
+to run script.
+
+Any errors should be logged in a new file in the 'logs' directory.
--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const LineByLineReader = require('line-by-line');
 const parse = require('csv-parse');
 const file = require('./components/file');
 
+// TODO: maybe move this to config or ENV file
 const config = {
   logFolder: __dirname + '/logs/',
   dataFolder: __dirname + '/data/',
-  fileName: 'lifesight6',
+  // TODO: filename should be read by script in order to automate
+  // filename without extension
+  fileName: '<filename>',
   timestamp: Date.now(),
 };
 
 file.openFile(config.logFolder, config.fileName, config.timestamp);
+// TODO: currently tsv is hardcoded, this should work with csv, too.
 const lr = new LineByLineReader(config.dataFolder + config.fileName + '.tsv');
 
 lr.on('error', function (err) {
@@ -25,8 +29,8 @@ lr.on('line', function (line) {
     const options = {
       host: 'aud.pubmatic.com',
       path:
-        //TODO: segid can be multiple
-        //TODO: IP needs to be added - country/IP logic, i.e. one has to be there, both can
+        // TODO: segid can be multiple
+        // TODO: IP needs to be added - country/IP logic, i.e. one has to be there, both can
         `/AdServer/Artemis?dpid=${userInfo[0]}&userid=${userInfo[1]}&segid=${userInfo[2]}&country=${userInfo[4]}&uidtype=${userInfo[5]}`,
       method: 'GET',
     };
